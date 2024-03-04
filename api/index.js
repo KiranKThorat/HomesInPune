707a2469f28e7472a59b04832f283ef2e5f1a3e4
kt <kthorat158@gmail.com>
2024-03-04 16:25:42 +0530

    resolved git error finnaly

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import UserRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
@@ -18,14 +19,15 @@ mongoose.connect(process.env.mongo).then(() => {
 const app= express();
 app.use(express.json());
 app.use(cookieParser());
-
+app.use('/api/listing', listingRouter);
 app.listen(3000,()=>{
 console.log("server started on port 3000.....");
 }
 );
 
 app.use('/api/user', UserRouter);
-app.use('/api/auth', authRouter);  
+app.use('/api/auth', authRouter); 
+ 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';

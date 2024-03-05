e3d6337ee9b105dac5020bbd2dc499d079265f2a
kt <kthorat158@gmail.com>
2024-03-05 15:22:50 +0530

    completed create listing page functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Home</span>
+            <span className='text-slate-500'>Homes</span>
             <span className='text-slate-700'>In</span>
             <span className='text-slate-700'>Pune</span>
 

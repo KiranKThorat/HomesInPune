9c94ac0092f254a0e6854d68157e2b1a271c3879
kt <kthorat158@gmail.com>
2024-03-05 18:33:31 +0530

    Completed show user listings on profile page functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,6 @@
 import express from 'express';
-import { test, updateUser } from '../controllers/user.controllers.js';
 import { verifyToken } from '../utils/verifyUser.js';
-import { deleteUser } from '../controllers/user.controllers.js';
+import { deleteUser, test, updateUser, getUserListings} from '../controllers/user.controllers.js';
 
 
 const router = express.Router();
@@ -9,5 +8,6 @@ const router = express.Router();
 router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
+router.get('/listings/:id', verifyToken, getUserListings)
 
 export default router;

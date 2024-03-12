96964cba67c7ab69c194655770a5dd2c3361f52e
kt <kthorat158@gmail.com>
2024-03-12 17:33:38 +0530

    handled small bug in listing page

--- client/src/Pages/CreateListing.jsx
+++ client/src/Pages/CreateListing.jsx
@@ -272,7 +272,7 @@ export default function CreateListing() {
                 type='number'
                 id='regularPrice'
                 min='1000'
-                max='50000000'
+                max='10000000'
                 required
                 className='p-3 border border-gray-300 rounded-lg'
                 onChange={handleChange}
